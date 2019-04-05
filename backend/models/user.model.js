c1a4efe56a1e5b656c02edd4f52ce225afe131e6
Marcoozvn <marcos.cesar@ccc.ufcg.edu.br>
2019-04-05 00:06:44 -0300

    Adding rating and user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
+const Schema   = mongoose.Schema;
+
+let UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        validate: {
+            validator: function(personalEmail) {
+                return new RegExp('^([_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*(\\.[a-zA-Z]{1,6}))?$').test(personalEmail)
+            },
+            message: 'Invalid email'
+        },
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    ratings: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Rating'
+    }]
+});
+
+UserSchema.pre('save', async function (next) {
+    if (this.password) {
+        const hash = await bcrypt.hash(this.password, 10);
+        this.password = hash;
+        next();
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
